7d235a12bca9fdcd107cea5600c0433ef755eb84
K1ngmar <ingmarsven.kole@gmail.com>
2024-02-07 17:04:05 +0100

    start of implementing tuple

---
+++ tuple/main.cpp
@@ -0,0 +1,9 @@
+
+#include "tuple.h"
+
+int main()
+{
+	km::Tuple a(1, "2", '3', 4.5f);
+
+	return 0;
+}
---
+++ tuple/tuple.h
@@ -0,0 +1,45 @@
+
+#pragma once
+
+#include <stdlib.h>
+#include <utility>
+
+namespace km
+{
+
+	template <size_t i, class T>
+	struct TupleItem
+	{
+		T item;
+
+		TupleItem(const T&& val) : item(std::forward(val))
+		{
+		}
+	};
+
+	template <class ...Ts>
+	struct Tuple__impl {};
+
+	template <size_t i, class T, class ...TrailingTypes>
+	struct Tuple__impl : public TupleItem<i, T>, public Tuple__impl<i + 1, TrailingTypes...>
+	{
+		Tuple__impl(const T&& val, TrailingTypes...) : TupleItem<i, T>(val)
+		{}
+	};
+
+	template <class ...Types>
+	class Tuple : Tuple__impl<0, Types...>
+	{
+		static constexpr size_t size = sizeof...(Types);
+
+	public:
+
+		/*!
+		 * @brief
+		*/
+		Tuple(const Types...&& values) : TupleItem(std::forward(values))
+		{
+		}
+	};
+
+}
